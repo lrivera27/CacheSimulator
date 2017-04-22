937ec6e6da7a5ecf692f9f8910cba3e5bcba1eb9
lrivera f <lrivera f>
2017-04-21 21:16:38 -0400

    Updated FIFO documentation

--- src/cachesimulator/FIFO.java
+++ src/cachesimulator/FIFO.java
@@ -1,4 +1,5 @@
 package cachesimulator;
+
 import java.util.ArrayList;
 
 /*
@@ -11,74 +12,107 @@ import java.util.ArrayList;
         from the Cache improving the algorithm's performance (Compared to other Examples).
 
         It relies on the following variables from the Cache class
-        1. cacheSize: This is the size of the cache.
-        2. ways: This is how many sets there are for the cache.
-        3. startTime: This is just the time in miliseconds created when Cache was first run.
-*/
-
+        1. _cacheSize: This is the size of the cache.
+        2. _ways: This is how many sets there are for the cache.
+        3. _startTime: This is just the time in miliseconds created when Cache was first run.
+        4. _numberOfBlocksPerSet: This is how many blocks there are per set of the cache.
+ */
 public class FIFO {
+
     // Holds the general FIFO details
     public int _hits;
     public int _misses;
     public long _endTime;
-    
+
     // Holds the cache that we will apliying FIFO's algorithm to.
     public ArrayList<Integer> _fifoCache = new ArrayList<>();
-    
-    // Holds the Array with the indexes of the next value to be replaced.
-    // The reason we need this to be an array is because this class works 
-    // for N-ways, so we need one index for each set.
+
+    /* 
+        Holds the Array with the indexes of the next value to be replaced.
+        The reason we need this to be an array is because this class works 
+        for N-ways, so we need one index for each set.
+    */
     public ArrayList<Integer> _cacheIndex = new ArrayList<>();
-    
-    // This Start Method will simply start the FIFO replacement algorithm.
-    // It takes as the parameter an instance of Cache as it relies on several
-    // variables explained uptop.
-    public void Start(Cache cacheDetails) { 
-        // Since we are using Dynamic Arrays, we need to fill them up so
-        // we can reference them later on.
-        for(byte i = 0; i < cacheDetails._cacheSize; i++){
+
+    /*  
+        This Start Method will simply start the FIFO replacement algorithm.
+        It takes as the parameter an instance of Cache as it relies on several
+        variables explained uptop.
+    */
+    public void Start(Cache cacheDetails) {
+        
+        /*  
+            Since we are using dynamic arrays, we need to fill them up so
+            we can reference them later on.
+        */
+        for (byte i = 0; i < cacheDetails._cacheSize; i++) {
+            
+            /* 
+                The reason we need to fill the cache with nulls is just so we dont
+                incorrectly find a hit with a 0.
+            */
             _fifoCache.add(null);
         }
-        for(byte i = 0; i < cacheDetails._ways; i++){
+        
+        for (byte i = 0; i < cacheDetails._ways; i++) {
             _cacheIndex.add(0);
         }
-        // Holds the index we are going to be performing FIFO on
-        int setIndex;
-        // Holds the address gathered from the list of addresses (This needs to be filtered in the Cache class first)
-        int address;
         
-        for(byte i = 0; i < cacheDetails._addresses.size(); i++) {
-            // Loads address at index i
-            address = cacheDetails._addresses.get(i);          
-            // Calculates which set we will be working on
-            setIndex = address % cacheDetails._ways;     
-            // Checks wether or not the address taken from the addresses is already in the cache
-            // if it is, it's a hit
-            if(_fifoCache.contains(address)) {
+        
+        //  Holds the index of the set we will be performing the replacement.
+        int setIndex;
+        
+        /*  
+            Holds the address from the array of address
+            since we will be using it multiple times.
+            This needs to be filtered in the Cache class first.
+        */
+        int address;
+
+        for (byte i = 0; i < cacheDetails._addresses.size(); i++) {
+            
+            //  Loads address at index i
+            address = cacheDetails._addresses.get(i);
+            
+            //  Calculates which set we will be working on
+            setIndex = address % cacheDetails._ways;
+            
+            /*
+                Checks wether or not the address taken from the addresses 
+                is already in the cache if it is, it's a hit
+            */
+            if (_fifoCache.contains(address)) {
                 _hits++;
                 System.out.println("HIT");
             } else {
-                // Checks wether the index for the set is more than the number of blocks per set
-                // since we dont want to be referencing content out of bounce.
-                if((_cacheIndex.get(setIndex)) >= cacheDetails._numberOfBlocksPerSet) {
-                   
+                
+                /* 
+                    Checks wether the index for the set is more than the number of blocks per set
+                    since we dont want to be referencing content out of bounce.
+                */ 
+                if ((_cacheIndex.get(setIndex)) >= cacheDetails._numberOfBlocksPerSet) {
+
                     _cacheIndex.set(setIndex, 0);
                 }
                 _misses++;
-                // To get the number we want to replace, we use a formula we created that goes like this:
-                // index I want + number of set the number is at times the number of blocks per set
-                // i + SetIndex * NumberOfBlocksPerSet
+                
+                /* 
+                    To get the number we want to replace, we use a formula we created that goes like this:
+                    index I want plus the number of set the number is at times the number of blocks per set
+                    i + SetIndex * NumberOfBlocksPerSet
+                */ 
                 _fifoCache.set(_cacheIndex.get(setIndex) + (setIndex * cacheDetails._numberOfBlocksPerSet), address);
-                _cacheIndex.set(setIndex, _cacheIndex.get(setIndex)+1);
+                _cacheIndex.set(setIndex, _cacheIndex.get(setIndex) + 1);
             }
             DisplayCache();
         }
         // This calculates the time it takes to finish the algorithm
         _endTime = System.currentTimeMillis() - cacheDetails._startTime;
     }
-    public void DisplayCache(){
+
+    public void DisplayCache() {
         System.out.println("_______");
-        for(byte i = 0; i < _fifoCache.size(); i++){
+        for (byte i = 0; i < _fifoCache.size(); i++) {
             System.out.print("|__");
             System.out.print(_fifoCache.get(i));
             System.out.println("__|");
@@ -86,4 +120,3 @@ public class FIFO {
         System.out.println("_______");
     }
 }
-
