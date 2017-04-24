42dd42c320ef66fef653e643f84ff2c34cd38056
lrivera f <lrivera f>
2017-04-24 02:30:17 -0400

    Finished LRU Documentation

--- src/cachesimulator/LRU.java
+++ src/cachesimulator/LRU.java
@@ -13,25 +13,62 @@ import java.util.ArrayList;
  */
 public class LRU {
 
+    //  Holds the general LRU details
     public int _hits;
     public int _misses;
     public long _endTime;
+    
+    //  Holds the cache that we will be applying LRU's algorithm to.
     public ArrayList<Integer> _lruCache = new ArrayList<>();
 
+    /*
+        This Start Method will simply start the LRU replacement algorithm.
+        It takes as a parameter an instance of Cache as it relies on several
+        variables explained uptop.
+    */
     public void Start(Cache cacheDetails) {
+        long startTime = System.currentTimeMillis();
+        
+        /*
+            Since we are using dynamic arrays, we need to fill them
+            up with nulls so we can reference them later on.
+        */
         for (byte i = 0; i < cacheDetails._cacheSize; i++) {
-            _lruCache.add(null);
+            _lruCache.add(-1);
         }
 
+        /*  
+            Holds the address from the array of address
+            since we will be using it multiple times.
+            This needs to be filtered in the Cache class first.
+        */
         int address;
+        
+        //  Holds the index of the set we will be performing the replacement.
         int setIndex;
+        
+        /* 
+            Holds the index of the address that had a hit.
+            Caution: This does NOT hold the real index, it still needs
+            to go through our formula: Index + Set * Blocks per set
+        */
         int indexOfHit;
 
+        /*
+            Since we have only one array, doing a regular linear search
+            in the array to find hits would mean our algorithm would be
+            really slow, so these two variables will hold the first and last
+            index of the set.
+        */
         int startingIndex;
         int endingIndex;
 
         for (byte i = 0; i < cacheDetails._addresses.size(); i++) {
+            
+            //  Loads address at index i
             address = cacheDetails._addresses.get(i);
+            
+            //  Calculates which set we will be working on.
             setIndex = address % cacheDetails._ways;
 
             indexOfHit = -1;
@@ -72,16 +109,35 @@ public class LRU {
             if (indexOfHit >= 0) {
                 _hits++;
 
+                /*
+                    This removes the element at the index of hit since we need
+                    have the most recently used element at the bottom. The remove 
+                    mathod also shifts the remainding elements to the left.
+                */
                 _lruCache.remove(indexOfHit);
+                
+                /*
+                    Since remove already shifts the values to the left, we just
+                    need to add the new address at the end of the list.
+                */
                 _lruCache.add((cacheDetails._numberOfBlocksPerSet - 1) + setIndex * cacheDetails._numberOfBlocksPerSet, address);
             } else {
                 _misses++;
 
+                /*
+                    This removes the element that was least recently used and 
+                    shifts the remainding elements of the list.
+                */
                 _lruCache.remove((setIndex * cacheDetails._numberOfBlocksPerSet));
+                
+                /*
+                    Since we want to keep the most recently used element at the bottom
+                    we just use the method add
+                */
                 _lruCache.add(((cacheDetails._numberOfBlocksPerSet - 1) + setIndex * cacheDetails._numberOfBlocksPerSet), address);
             }
         }
 
-        _endTime = System.currentTimeMillis() - cacheDetails._startTime;
+        _endTime = System.currentTimeMillis() - startTime;
     }
 }
